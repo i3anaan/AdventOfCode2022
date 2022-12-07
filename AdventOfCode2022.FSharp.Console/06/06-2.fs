5917b62222966693cabfba30aacedd32421e13f1
Derk Snijders <i3anaan@gmail.com>
2022-12-07 21:58:07 +0100

    feat: day 6 part 2

--- AdventOfCode2022.FSharp.Console/06/06-2.fs
+++ AdventOfCode2022.FSharp.Console/06/06-2.fs
@@ -4,15 +4,15 @@ let readLines filePath = System.IO.File.ReadLines(filePath)
 let lines = readLines "C:\Users\Eigenaar\Projects\AdventOfCode2022\AdventOfCode2022.FSharp.Console\06\input.txt"
 let line = (Seq.toList lines)[0]
 
-let rec findUnique4 (index: int) (buffer: list<char>) (remaining: list<char>): int =
+let rec findUnique14 (index: int) (buffer: list<char>) (remaining: list<char>): int =
     printfn "index: %d buffer: %A remaining: %O" index buffer remaining
     match List.length buffer with
-    | x when x < 4 -> findUnique4 (index + 1) ([List.head remaining] @ buffer) (List.tail remaining)
+    | x when x < 14 -> findUnique14 (index + 1) ([List.head remaining] @ buffer) (List.tail remaining)
     | x ->  match (List.distinct buffer |> List.length) with
-            | x when x < 4 -> findUnique4 (index + 1) ([List.head remaining] @ (List.splitAt 3 buffer |> fst)) (List.tail remaining)
-            | x when x >= 4 -> index
+            | x when x < 14 -> findUnique14 (index + 1) ([List.head remaining] @ (List.splitAt 13 buffer |> fst)) (List.tail remaining)
+            | x when x >= 14 -> index
             | _ -> failwith "?"
 
 Seq.toList line
-|> findUnique4 0 [] 
+|> findUnique14 0 [] 
 
